1419b78c8a1478a72d5c36bb521a59733eaf7943
Po Co <polniolino@gmail.com>
2024-09-04 11:07:12 +0200

    Add rust snippet

--- queries/lua/injections.scm
+++ queries/lua/injections.scm
@@ -1,4 +1,17 @@
-(
- (string_content) @query
- (#match? @query ";; qq")
- )
+; extends
+
+((comment) @injection.language
+ [
+  (expression_list (string (string_content) @injection.content))
+  (string (string_content) @injection.content)
+ ]
+ (#offset! @injection.language 0 5 0 -5))
+
+((string_content) @injection.content
+ (#match? @injection.content "(SELECT|INSERT|UPDATE|DELETE|CREATE).+(FROM|INTO|VALUES|SET|TABLE).*(WHERE|GROUP BY)?")
+ (#set! injection.language "sql"))
+
+; (
+;  (string_content) @query
+;  (#match? @query ";; qq")
+;  )
